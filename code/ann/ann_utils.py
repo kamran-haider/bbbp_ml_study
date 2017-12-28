cf989846a82e5ca208e18ea80ecf7bb199f44c1d
kamran haider <kamranhaider.mb@gmail.com>
2017-12-28 17:38:58 -0500

    added dataset for testing ann implementation

--- code/ann/ann_utils.py
+++ code/ann/ann_utils.py
@@ -2,6 +2,7 @@
 Utility functions for ann module.
 """
 import numpy as np
+import h5py
 
 
 def sigmoid(z):
@@ -94,3 +95,20 @@ def sigmoid_backward(dA, cache):
     assert (dZ.shape == Z.shape)
 
     return dZ
+
+
+def _load_test_data():
+    train_dataset = h5py.File('tests/test_datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:])  # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:])  # your train set labels
+
+    test_dataset = h5py.File('tests/test_datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:])  # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:])  # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:])  # the list of classes
+
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
